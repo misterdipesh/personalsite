4ddf81553dfefb545492825c2945f671ef7575aa
Dipesh Bishwakarma <deepeesh471@gmail.com>
2020-12-07 19:13:26 +0545

    running version

---
+++ src/pages/about/about.component.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./about.styles.css";
+import Profile from "../../assets/img/profile/profile.webp";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+
+const About = () => {
+  return (
+    <div id="about">
+      <div className="about">
+        <h1 className="pt-3 text-center font-details pb-3">ABOUT ME</h1>
+        <Container>
+          <Row className="pt-3 pb-5 align-items-center">
+            <Col xs={12} md={6}>
+              <Row className="justify-content-center mb-2 mr-2 ">
+                <Image className="rounded-circle profile"
+				  alt="100x100" src={Profile} thumbnail fluid />
+              </Row>
+            </Col>
+            <Col xs={12} md={6}>
+              <Row className=" align-items-start p-2 my-details rounded">
+                Hi there! I am <strong>&nbsp;Dipesh Bishwakarma</strong>
+                <br />A passionate programmer interested in AI and releated technologies. I love learning about new technologies.
+                 I am currently doing my Bachelor of Science in Computer Science and Information Technology.
+                Along with that, I am also a mountainbike guide working at Pokhara Mountainbike Adventure.
+				I love learning about new technologies, what problems are they solving and How can I use them to build better and scalable products.
+                <br /> <br />
+                <Col className="d-flex justify-content-center flex-wrap">
+                  <div>
+                    <a href="#contact">
+                      <Button className="m-2" variant="outline-primary">
+                        Let's talk
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://drive.google.com/file/d/19ugbpqI9UAeX8duCs4ShJDKwPQF7oWpU/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-success">
+                        My Resume
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://github.com/misterdipesh" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-light">
+                        GitHub
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://www.linkedin.com/in/misterdipesh" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-info">
+                        LinkedIn
+                      </Button>
+                    </a>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default About;
